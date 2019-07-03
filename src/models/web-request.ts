513a7c6753df1d6540d831f25acf1205e93fa5ed
Sential <sentialx@gmail.com>
2019-07-04 01:17:35 +0200

    fix: multiple webRequest events

--- src/models/web-request.ts
+++ src/models/web-request.ts
@@ -386,6 +386,25 @@ const aliasHandler = {
   },
 };
 
-export default (session: Session) => {
+const betterWebRequest = (session: Session) => {
   return new Proxy(new BetterWebRequest(session.webRequest), aliasHandler);
 };
+
+const store = new Set();
+
+const enhanceWebRequest = (session: Session): Session => {
+  if (store.has(session)) {
+    return session;
+  }
+
+  Object.defineProperty(session, 'webRequest', {
+    value: betterWebRequest(session),
+    writable: false,
+  });
+
+  store.add(session);
+
+  return session;
+};
+
+export default enhanceWebRequest;

15071770cdfc8f0921cda7c0bf1667a8abb69393
Sential <sentialx@gmail.com>
2019-07-03 19:00:26 +0200

    feat: add event types

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -1,6 +1,17 @@
 import { EventEmitter } from 'events';
 import { ipcRenderer, IpcMessageEvent } from 'electron';
 
+export declare interface ExtensionsRenderer {
+  on(
+    event: 'create-tab',
+    listener: (
+      details: chrome.tabs.CreateProperties,
+      callback: (tabId: number) => void,
+    ) => void,
+  ): this;
+  on(event: string, listener: Function): this;
+}
+
 export class ExtensionsRenderer extends EventEmitter {
   constructor() {
     super();

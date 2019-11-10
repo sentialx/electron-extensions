aa79d969e8d1251b69f5124c42dd7be34f78218c
Sential <sentialx@gmail.com>
2019-11-10 19:31:42 +0100

    test: add port tests

--- test/extension/background.js
+++ test/extension/background.js
@@ -7,6 +7,14 @@ const tests = [
     status: false,
     msg: 'Receiving message from content script',
   },
+  {
+    status: false,
+    msg: 'Receiving port from content script',
+  },
+  {
+    status: false,
+    msg: 'Receiving port message from content script',
+  },
 ];
 
 setTimeout(() => {
@@ -20,6 +28,16 @@ setTimeout(() => {
 chrome.runtime.onMessage.addListener((res, sender, sendResponse) => {
   if (res === 'test' && sender.tab.id === 1) tests[1].status = true;
   sendResponse('test');
+});
+
+chrome.runtime.onConnect.addListener(port => {
+  if (port.name === 'testname') tests[2].status = true;
+
+  port.postMessage('test');
+
+  port.onMessage.addListener(msg => {
+    if (msg === 'test') tests[3].status = true;
+  });
 });
 
 setTimeout(() => {
--- test/extension/content.js
+++ test/extension/content.js
@@ -7,11 +7,22 @@ const tests = [
     status: false,
     msg: 'Receiving message from background',
   },
+  {
+    status: false,
+    msg: 'Receiving port message from background',
+  },
 ];
 
 setTimeout(() => {
   chrome.runtime.sendMessage('test', res => {
     if (res === 'test') tests[0].status = true;
+  });
+
+  const port = chrome.runtime.connect({ name: 'testname' });
+  port.postMessage('test');
+
+  port.onMessage.addListener(msg => {
+    if (msg === 'test') tests[2].status = true;
   });
 }, 100);
 

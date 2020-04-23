e42cac13ff7aa0296d2b2a36dad3932e68b772d0
sentialx <sentialx@gmail.com>
2020-04-23 20:33:40 +0200

    refactor: background pages

--- src/browser/background-pages.ts
+++ src/browser/background-pages.ts
@@ -1,9 +1,8 @@
-import { session, app, webContents } from 'electron';
+import { app, webContents } from 'electron';
 import { parse } from 'url';
 import { EventEmitter } from 'events';
 import { PROTOCOL_SCHEME } from '../constants';
-
-const backgroundPages: Set<Electron.WebContents> = new Set();
+import { isBackgroundPage } from '../utils/web-contents';
 
 export const sendToExtensionPages = (channel: string, ...args: any[]) => {
   webContents.getAllWebContents().forEach((wc) => {
@@ -12,53 +11,33 @@ export const sendToExtensionPages = (channel: string, ...args: any[]) => {
   });
 };
 
-export const registerBackgroundPage = (host: Electron.WebContents) => {
-  backgroundPages.add(host);
-
-  host.once('destroyed', () => {
-    backgroundPages.delete(host);
-  });
-};
-
-export const getBackgroundPage = (
-  extensionId: string,
-  ses: Electron.Session = null,
-) => {
-  if (!ses) ses = session.defaultSession;
-
-  return Array.from(backgroundPages).find(
-    (x) => parse(x.getURL()).hostname === extensionId && x.session === ses,
-  );
-};
-
 export declare interface BackgroundPages {
   on(
-    event: 'register',
+    event: 'created',
     listener: (webContents: Electron.WebContents) => void,
   ): this;
   on(event: string, listener: Function): this;
 }
 
 export class BackgroundPages extends EventEmitter {
-  public observe() {
-    app.on('web-contents-created', (e, webContents) => {
-      // TODO: https://github.com/electron/electron/pull/22217
-      // if (webContents.getType() === 'backgroundPage') {
-      if (
-        webContents.getType() === 'remote' &&
-        webContents.getURL().startsWith(PROTOCOL_SCHEME)
-      ) {
-        this.register(webContents);
-        this.emit('register', webContents);
+  public constructor() {
+    super();
+
+    app.on('web-contents-created', (e, wc) => {
+      if (isBackgroundPage(wc)) {
+        this.emit('created', webContents);
       }
     });
   }
 
   public get(extensionId: string, session: Electron.Session) {
-    return getBackgroundPage(extensionId, session);
-  }
-
-  public register(webContents: Electron.WebContents) {
-    registerBackgroundPage(webContents);
+    return webContents
+      .getAllWebContents()
+      .find(
+        (x) =>
+          isBackgroundPage(x) &&
+          parse(x.getURL()).hostname === extensionId &&
+          x.session === session,
+      );
   }
 }
